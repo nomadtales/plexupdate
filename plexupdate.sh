6c5477cda9e42ec07fcfddd61abebc8da36efa32
NomadTales <nomadtales@gmail.com>
2022-12-08 08:20:56 +1100

    add logging

--- plexupdate.sh
+++ plexupdate.sh
@@ -4,30 +4,48 @@
 #
 # Must be run as root.
 #
+# Forked from:
 # @author @martinorob https://github.com/martinorob
 # https://github.com/martinorob/plexupdate/
 
-#!/bin/bash
+echo `date +"%Y-%m-%d %T"` - Stopping Plex Media Server
+/usr/syno/bin/synopkg stop "Plex Media Server"
+
 mkdir -p /tmp/plex/ > /dev/null 2>&1
+
 token=$(cat /volume1/Plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
 url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
 jq=$(curl -s ${url})
+
 newversion=$(echo $jq | jq -r .nas.Synology.version)
-echo New Ver: $newversion
+echo `date +"%Y-%m-%d %T"` - New Version: $newversion
+
 curversion=$(synopkg version "Plex Media Server")
-echo Cur Ver: $curversion
+echo `date +"%Y-%m-%d %T"` - Currrent Version: $curversion
+
 if [ "$newversion" != "$curversion" ]
 then
-echo New Vers Available
+echo `date +"%Y-%m-%d %T"` - New Version Available
 /usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
 CPU=$(uname -m)
 url=$(echo "${jq}" | jq -r '.nas.Synology.releases[] | select(.build=="linux-'"${CPU}"'") | .url')
+
+echo `date +"%Y-%m-%d %T"` - Downloading New Version
 /bin/wget $url -P /tmp/plex/
+
+echo `date +"%Y-%m-%d %T"` - Installing New Version
 /usr/syno/bin/synopkg install /tmp/plex/*.spk
+
 sleep 30
-/usr/syno/bin/synopkg start "Plex Media Server"
+
+echo `date +"%Y-%m-%d %T"` - Removing Temp Files
 rm -rf /tmp/plex/*
 else
-echo No New Ver
+
+echo `date +"%Y-%m-%d %T"` - No New Version
 fi
+
+echo `date +"%Y-%m-%d %T"` - Starting Plex Media Server
+/usr/syno/bin/synopkg start "Plex Media Server"
+
 exit
